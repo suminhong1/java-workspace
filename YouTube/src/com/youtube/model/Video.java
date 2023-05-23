61cf6aae66068fba7335ed03fc634c527eed4d13
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-05-23 15:16:12 +0900

    Update Video.java

--- YouTube/src/com/youtube/model/Video.java
+++ YouTube/src/com/youtube/model/Video.java
@@ -1,5 +1,40 @@
 package com.youtube.model;
 
+import java.util.Date;
+
 public class Video {
 
+	public String title;
+	public String commnet;
+	public Date uploadAt;
+	public int count; // views
+	public String imgUrl;
+	public String fileUrl;
+	
+	/*
+	 * Create : 추가
+	 * Read : 읽기(1개, 목록)
+	 * Update : 수정
+	 * Delete: 삭제
+	 */
+	
+	public void upload() {// 영상 업로드
+		
+	}
+	
+	public void videoList() {// 동영상 목록
+		
+	}
+	
+	public void viewVideo() {// 동영상 수정
+		
+	}
+	public void deleteVideo() {//동영상 삭제
+		
+	}
+	// 영상 업로드
+	// 영상 목록
+	// 영상 1개 보기
+	// 동영상 수정
+	// 동영상 삭제
 }
