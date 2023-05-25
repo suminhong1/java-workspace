6f58429cdfa61b30c114193b0fa071dd3c8290f1
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-05-25 15:20:30 +0900

    20230525

--- YouTube/src/com/youtube/controller/VideoController.java
+++ YouTube/src/com/youtube/controller/VideoController.java
@@ -1,5 +1,35 @@
 package com.youtube.controller;
 
-public class VideoController {
+import com.youtube.model.Video;
 
+public class VideoController {
+	
+	/*
+	 * Create : 추가
+	 * Read : 읽기(1개, 목록)
+	 * Update : 수정
+	 * Delete: 삭제
+	 */
+	
+	public Video upload() { // 영상 업로드
+		return null;
+	}
+	public Video[] videoList() { // 동영상 목록
+		return null;
+	}
+	public Video viewVideo() { // 영상 1개보기
+		return null;
+	}
+	public Video updateVideo() {//동영상 수정
+		return null;
+	}
+	public boolean deleteVideo() { //동영상 삭제
+		return false;
+	}
+
+	// 영상 업로드
+	// 영상 목록
+	// 영상 1개 보기
+	// 동영상 수정
+	// 동영상 삭제
 }
